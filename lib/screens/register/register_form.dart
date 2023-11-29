a062f08f66da1f043291a6605c09337e250bad66
Samit Koyom <samitkoyom@gmail.com>
2023-11-29 23:43:55 +0700

    finished day 3

--- lib/screens/register/register_form.dart
+++ lib/screens/register/register_form.dart
@@ -1,8 +1,13 @@
 // ignore_for_file: use_build_context_synchronously, prefer_const_constructors
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_node_store/app_router.dart';
 import 'package:flutter_node_store/components/custom_textfield.dart';
 import 'package:flutter_node_store/components/rounded_button.dart';
+import 'package:flutter_node_store/main.dart';
+import 'package:flutter_node_store/services/rest_api.dart';
+import 'package:flutter_node_store/utils/utility.dart';
 
 class RegisterForm extends StatelessWidget {
   RegisterForm({super.key});
@@ -124,10 +129,51 @@ class RegisterForm extends StatelessWidget {
                       _formKeyRegister.currentState!.save();
 
                       // แสดงข้อมูลที่บันทึกได้ทาง Console
-                      print("First Name: ${_firstNameController.text}");
-                      print("Last Name: ${_lastNameController.text}");
-                      print("Email: ${_emailController.text}");
-                      print("Password: ${_passwordController.text}");
+                      // print("First Name: ${_firstNameController.text}");
+                      // print("Last Name: ${_lastNameController.text}");
+                      // print("Email: ${_emailController.text}");
+                      // print("Password: ${_passwordController.text}");
+
+                      // เรียกใช้งาน API สำหรับลงทะเบียน Register
+                      var response = await CallAPI().registerAPI(
+                        {
+                          "firstname": _firstNameController.text,
+                          "lastname": _lastNameController.text,
+                          "email": _emailController.text,
+                          "password": _passwordController.text
+                        }
+                      );
+
+                      var body = jsonDecode(response);
+
+                      logger.i(body);
+
+                      if(body['message'] == 'No Network Connection'){
+                        // แจ้งเตือนว่าไม่มีการเชื่อมต่อ Internet
+                        Utility.showAlertDialog(
+                          context, 
+                          'แจ้งเตือน', 
+                          body['message']
+                        );
+                      }else{
+                        if(body['status'] == 'ok'){
+                          // แจ้งเตือนว่าลงทะเบียนสำเร็จ
+                          // Utility.showAlertDialog(
+                          //   context, 
+                          //   'แจ้งเตือน', 
+                          //   body['message']
+                          // );
+                          // ส่งกลับไปหน้า Login
+                          Navigator.pushReplacementNamed(context, AppRouter.login);
+                        }else{
+                          // แจ้งเตือนว่าลงทะเบียนไม่สำเร็จ
+                          Utility.showAlertDialog(
+                            context, 
+                            'แจ้งเตือน', 
+                            body['message']
+                          );
+                        }
+                      }
                       
                     }
                   },
--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_node_store/app_router.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -17,6 +18,10 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 
   // ฟังก์ชันเมื่อจบการแสดง Intro
   void _onIntroEnd(context) async {
+
+    // Set ค่าให้กับ SharedPreferences เพื่อบอกว่าเคยแสดง Intro แล้ว
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('welcomeStatus', true);
 
     // ไปยังหน้า Login แบบเปิดซ้อนทับหน้าเดิม
     // Navigator.pushNamed(context, AppRouter.login); // เปิดแบบปกติมีปุ่มย้อนกลับ
